bd76fb3f6b14771fda3d48f7720d75a3937a46d4
Maximo <maximo.vieyra@gmail.com>
2022-08-18 11:10:27 -0300

    Desafio deno

---
+++ Desafio-21/index.ts
@@ -0,0 +1,22 @@
+import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+
+const port = 8080;
+
+localStorage.clear();
+
+const requestHandler = async (req: Request): Promise<Response> => {
+    let colors: string[] = [];
+    if (localStorage.getItem("colors")) {
+        colors = JSON.parse(localStorage.getItem("colors") || "");
+    }
+    if (req.method === "POST") {
+        const { color } = await req.json();
+        colors.push(color);
+    }
+    localStorage.setItem("colors", JSON.stringify(colors));
+    return new Response(JSON.stringify(colors));
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:3000/`);
+
+await serve(requestHandler, { port });
